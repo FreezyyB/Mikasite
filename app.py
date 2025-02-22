9a270667469f21b67ff2f72ac2ce30991ec79ab5
FreezyyB <132308601+FreezyyB@users.noreply.github.com>
2025-02-22 18:27:38 +0000

    Update app.py
    Edited to take lines from new git file, as opposed to extracting from google doc file
    - faster data exfiltration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, session
 import requests
+import random
 
 app = Flask(__name__)
 app.secret_key = "Snowdrop"
@@ -8,7 +9,7 @@ app.secret_key = "Snowdrop"
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_PERMANENT"] = False
 
-GOOGLE_SCRIPT_URL = "1kQcyzPScpeIJgwMm9cbP10G0oiZLPUmVixnZSUP_z7n5vPmkZ2SFE8MA"
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/FreezyyB/Mikasite/refs/heads/main/lines.txt"
 
 # Preset login credentials
 USERNAME = "Mika"
@@ -27,8 +28,17 @@ def get_random_line():
     if not session.get("logged_in"):
         return jsonify({"error": "Unauthorized"}), 401
 
-    response = requests.get(GOOGLE_SCRIPT_URL)
-    return jsonify({"line": response.text})
+    response = requests.get(GITHUB_RAW_URL)
+
+    if response.status_code != 200:
+        return jsonify({"error": "Could not fetch data"}), 500
+    
+    lines = response.text.strip().slpit("\n")
+    if not lines:
+        return jsonify({"error": "No lines found"}), 500
+    
+    random_index = random.randint(0, len(lines) - 1)
+    return jsonify({"line:" lines[random_index]})
 
 @app.route('/logout', methods=['POST'])
 def logout():
